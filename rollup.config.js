af32174bb5fa0d704da11b6e43b11983583cef1d
Vinni <vinaus79@gmail.com>
2024-05-18 19:45:05 +0530

    code optimization

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
 import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
+
 export default [
   {
     input: "src/index.ts",
@@ -22,7 +24,8 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json", exclude: ["**/test/**"] }),//exclude test folder
+      typescript({ tsconfig: "./tsconfig.json", exclude: ["**/test/**"] }),
+      terser(), // Minify the output
     ],
   },
   {
